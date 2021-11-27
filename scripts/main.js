fece2fde13af2012a19d9ddde6019db6b44d1ebb
Mr_Spade <Mr_Spade@foxmail.com>
2021-11-27 21:10:31 +0800

    build

--- scripts/main.js
+++ scripts/main.js
@@ -4,10 +4,34 @@ let sig=[
   "世人苦被明日累，秋天漠漠向昏黑"
 ];
 
-document.querySelector('.signature').textContent=sig[Math.floor(Math.random()*sig.length)];
+document.querySelector(".signature").textContent=sig[Math.floor(Math.random()*sig.length)];
 //随机一条签名
 
 $(document).ready(function(){$(".toggle").next().hide();});
 //侧边列表默认收缩
 $(document).ready(function(){$(".toggle").click(function(){$(this).next().slideToggle();});});
 //侧边列表点击可以展开/收缩子项
+
+document.querySelector(".title-img").onclick=function()
+{
+  if(location.pathname.length>=2&&location.pathname[1]==='F')//本地测试
+  {
+    location.pathname="/F:/Mr-Spade.github.io/index.html";
+  }
+  else//网页
+  {
+    location.pathname="/";
+  }
+};
+
+document.querySelector(".b-OI").onclick=function()
+{
+  if(location.pathname.length>=2&&location.pathname[1]==='F')//本地测试
+  {
+    location.pathname="/F:/Mr-Spade.github.io/study/OI/index.html";
+  }
+  else//网页
+  {
+    location.pathname="/study/OI";
+  }
+};
