b1472bd839194a46dc3e27042e7c45d1dc81a8e5
Mr_Spade <Mr_Spade@foxmail.com>
2021-11-27 19:25:58 +0800

    building

--- scripts/main.js
+++ scripts/main.js
@@ -1,56 +1,10 @@
-let myImage = document.querySelector('img');
+let sig=[
+  "从一缕微光开始",
+  "我心中那团青色的怒火，终将燃烧这片大地",
+  "世人苦被明日累，秋天漠漠向昏黑"
+];
 
-myImage.onclick = function() {
-    let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/kx.jpg') {
-      myImage.setAttribute('src', 'images/mess.png');
-    } else {
-      myImage.setAttribute('src', 'images/kx.jpg');
-    }
-}
+document.querySelector('.signature').textContent=sig[Math.floor(Math.random()*sig.length)];
+//随机一条签名
 
-let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1');
-
-function setUserName() {
-    let myName = prompt('请输入你的名字。');
-    localStorage.setItem('name', myName);
-    myHeading.textContent = '空弦酷毙了，' + myName;
-  }
-if(!localStorage.getItem('name')) {
-  setUserName();
-} else {
-  let storedName = localStorage.getItem('name');
-  myHeading.textContent = '空弦酷毙了，' + storedName;
-}
-
-myButton.onclick = function() {
-  setUserName();
-}
-
-
-// function reset_height(e)
-// {
-  //if(e.contentDocument.body.clientHeight)
-  //  e.height=1000;
-  //e.src="article/art2.html";
-  // let e=document.querySelector('.article');
-  // e.height = e.contentDocument.body.scrollHeight;
-  // let res=document.querySelector('.result');
-  // res.textContent=location.pathname;
-// }
-
-// let my_article=document.querySelectorAll('.article');
-// for(let i=0;i<my_article.length;i++)
-// {
-//   reset_height(my_article[i]);
-// }
-let bd=document.querySelector('body');
-let art=document.createElement('iframe');
-art.width="80%";
-art.className='article';
-art.src="article/art.html";
-art.onload=function(){
-  art.height=art.contentDocument.body.scrollHeight;
-}
-bd.appendChild(art);
+//$(document).ready(function(){$(".title-img").click(function(){$(".signature").slideToggle();});});
